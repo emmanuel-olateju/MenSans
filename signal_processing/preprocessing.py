2cb128473d2d778dfbde42d627d4780ddcd8c8bc
emmanuel-olateju <olatejuemmanuel@gmail.com>
2024-01-09 22:05:26 +0100

    signal processing

---
+++ signal_processing/preprocessing.py
@@ -0,0 +1,28 @@
+import mne
+
+channels_map = {
+  'EEG 1':'Fp1',
+  'EEG 2':'Fp2',
+  'EEG 3':'C3',
+  'EEG 4':'C4',
+  'EEG 5':'T5',
+  'EEG 6':'T6',
+  'EEG 7':'O1',
+  'EEG 8':'O2',
+  'EEG 9':'F7',
+  'EEG 10':'F8',
+  'EEG 11':'F3',
+  'EEG 12':'F4',
+  'EEG 13':'T3',
+  'EEG 14':'T4',
+  'EEG 15':'P3',
+  'EEG 16':'P4'
+}
+
+def drop_accelerometer_channels(raw):
+  raw.drop_channels(['Accel X','Accel Y','Accel Z'])
+  return raw
+
+def rename_channels(raw):
+  raw.rename_channels(channels_map)
+  return raw
