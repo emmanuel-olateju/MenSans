9631cae7652006e2557760937c4e57dd9eab587b
emmanuel-olateju <olatejuemmanuel@gmail.com>
2024-01-31 22:25:13 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='MenSans',
+    version='0.1.0',    
+    description='MNE based EEg menta health analysis package',
+    url='https://github.com/emmanuel-olateju/MenSans',
+    author='Emmanuel Olateju',
+    author_email='oltejuemmanuel@gmail.com',
+    license='BSD 2-clause',
+    packages=['MenSans'],
+    install_requires=[
+        'mne==1.6.0',  
+        'matplotlib==3.8.2'                  
+        ],
+    classifiers=[
+    ],
+)
