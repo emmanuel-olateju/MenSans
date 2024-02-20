222c9ce1012cba8e5b3ffe2225f20cc9099aa3cd
emmanuel-olateju <olatejuemmanuel@gmail.com>
2024-02-20 14:59:33 +0100

    features computation test update: 11 passed, 18 warnings

--- features_computation/test_features.py
+++ features_computation/test_features.py
@@ -2,15 +2,30 @@ from .frequency import *
 from .time import *
 import pytest
 
-@pytest.fixture
-def hjorth_segment_size():
-    return 10
 
 @pytest.mark.parametrize("method_, avg_type_", [("welch","mean"),("welch","median"),("medfilt","mean"),("medfilt","median")])
 def test_band_power_method(eeg_data,sampling_frequency,bands,method_,avg_type_):
     spectrum = band_power(eeg_data,sampling_frequency,bands[1],method_,avg_type_)
-    assert np.iscomplexobj(spectrum) == False
+    assert np.iscomplexobj(spectrum)==False
     assert spectrum.ndim == 1
+
+@pytest.mark.parametrize("method_, avg_type_", [("welch","mean"),("welch","median"),("medfilt","mean"),("medfilt","median")])
+def test_bands_power(eeg_data,sampling_frequency,bands,method_,avg_type_):
+    bands_power_ = bands_power(eeg_data,sampling_frequency,bands,method_,avg_type_)
+    assert np.iscomplexobj(bands_power_)==False
+    assert bands_power_.ndim<=2 & bands_power_.ndim!=0
+    assert bands_power_.shape[-1]==len(bands)
+    if bands_power_.ndim==2:
+        assert bands_power_.shape[0]==eeg_data.shape[0]
+
+def test_compute_psd(eeg_data,sampling_frequency):
+    spectrum, freqs = compute_psd(eeg_data,sampling_frequency)
+    assert spectrum.ndim==eeg_data.ndim
+    assert spectrum.shape[-1]==freqs.shape[0]
+
+@pytest.fixture
+def hjorth_segment_size():
+    return 10
 
 def test_hjorth_method(eeg_data,hjorth_segment_size):
     hjorth_results = hjorth_parameters_computation(eeg_data,hjorth_segment_size)
